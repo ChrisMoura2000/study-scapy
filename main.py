e25c0c69d7322be23e77a7a972a774e2c372b7e3
ChrisMoura2000 <chris.tryber@gmail.com>
2026-04-05 22:18:34 -0300

    primeira etapa TCP

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from scapy.all import sniff, Ether, IP, TCP, UDP
+from scapy.all import sniff, Ether, IP, TCP
+from datetime import datetime
 
 def handle_packet(pkt):
-    if IP in pkt:
-        print("===============================")
-        print(pkt[Ether].dst)
-        print(pkt[IP].dst)
-        print(pkt[IP].proto)
+    data_formatada = datetime.now().strftime('%H:%M:%S')
+    print(data_formatada, end=" | ")
+    version_IP = {2048: "IPv4", 34525: "IPv6"}
+    if IP in pkt:  
+        if pkt[IP].proto == 6:
+            ip_version = version_IP[pkt[Ether].type]
+            source = pkt[IP].src
+            dest = pkt[IP].dst
+            srcport = int(pkt[TCP].sport)
+            dstport = int(pkt[TCP].dport)
+            seq = pkt[TCP].seq
+            ack = pkt[TCP].ack
+            flag = pkt[TCP].flags
+            print(f"{ip_version} {source}:{srcport} > {dest}:{dstport} # TCP seq:{seq} ack:{ack} flag:{flag}")
 
-sniff(prn=handle_packet ,count=10, filter="ip")
-
+sniff(prn=handle_packet, store=0, count=0, filter='ip')
